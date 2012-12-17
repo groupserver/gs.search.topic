60cea56d43d8f558360e823d0fa04d2a7ebfc7ca
Michael JasonSmith <mpj17@onlinegroups.net>
2012-12-17 14:51:28 +1100

    Whitespace and comma changes in the JavaScript.

--- gs/search/topic/browser/javascript/loadtopic.js
+++ gs/search/topic/browser/javascript/loadtopic.js
@@ -1,33 +1,33 @@
 jQuery.noConflict() 
 var GSSearchTopicsSiteHomepage = function () {
     var go_one_col = function () {
-	var left = null;
+        var left = null;
 
-	left = jQuery('#gs-site-home-left');
-	left.removeAttr('class');
+        left = jQuery('#gs-site-home-left');
+        left.removeAttr('class');
     };
     var handle_load = function(event) {
-	var results = null;
-	var nResults = null;
-	var right = null;
+        var results = null;
+        var nResults = null;
+        var right = null;
 
-	results = jQuery(event.target);
-	nResults = results.find('.gs-search-result').length;
-	if (nResults === 0) {
+        results = jQuery(event.target);
+        nResults = results.find('.gs-search-result').length;
+        if (nResults === 0) {
             right = jQuery('#gs-site-home-right');
             right.fadeOut('slow', 'swing', go_one_col);
-	}
+        }
     };
     
     return {
-	init: function() {
-	    var searchId = '#gs-search-topic-site-home-search'
-	    var topicSearch = GSSearch(searchId, '/s/search.ajax', 0, 6, {}, 
-				       null);
-	    topicSearch.load();
-	    jQuery(searchId).bind(topicSearch.RESULTS_LOADED_EVENT,
-				  handle_load);
-	},//init
+        init: function() {
+            var searchId = '#gs-search-topic-site-home-search'
+            var topicSearch = GSSearch(searchId, '/s/search.ajax', 0, 6, {}, 
+                                       null);
+            topicSearch.load();
+            jQuery(searchId).bind(topicSearch.RESULTS_LOADED_EVENT,
+                                  handle_load);
+        }//init
     }
 }();
 jQuery(document).ready( function () {
