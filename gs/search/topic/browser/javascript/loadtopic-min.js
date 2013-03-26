15cd5e99eb1b7b0f89d228e502064ff9dc006685
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-26 15:12:46 +1100

    Adding a minified version of the Load Topics script.

---
+++ gs/search/topic/browser/javascript/loadtopic-min.js
@@ -0,0 +1,4 @@
+jQuery.noConflict();function gs_search_topics_site_homepage(){var b="#gs-search-topic-site-home-search",a=null;
+a=GSSearch(b,"/s/search.ajax",0,10,{},null);jQuery("#gs-site-home-activity-tabs li:first-child a").click();
+a.load()}jQuery(document).ready(function(){gsJsLoader.with_module("/++resource++gs-search-base-js-min-20121217.js",gs_search_topics_site_homepage)
+});
