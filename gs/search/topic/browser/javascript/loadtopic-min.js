d57475b63fdd4f4143955436571928d234b035c7
Michael JasonSmith <mpj17@onlinegroups.net>
2014-03-14 11:27:51 -0700

    Turing on strict-mode, and following the update to gs.search.base.

--- gs/search/topic/browser/javascript/loadtopic-min.js
+++ gs/search/topic/browser/javascript/loadtopic-min.js
@@ -1,4 +1,4 @@
-jQuery.noConflict();function gs_search_topics_site_homepage(){var b="#gs-search-topic-site-home-search",a=null;
+"use strict";jQuery.noConflict();function gs_search_topics_site_homepage(){var b="#gs-search-topic-site-home-search",a=null;
 a=GSSearch(b,"/s/search.ajax",0,10,{},null);jQuery("#gs-site-home-activity-tabs li:first-child a").click();
-a.load()}jQuery(document).ready(function(){gsJsLoader.with_module("/++resource++gs-search-base-js-min-20131121.js",gs_search_topics_site_homepage)
+a.load()}jQuery(document).ready(function(){gsJsLoader.with_module("/++resource++gs-search-base-js-min-20140313.js",gs_search_topics_site_homepage)
 });
--- gs/search/topic/browser/javascript/loadtopic.js
+++ gs/search/topic/browser/javascript/loadtopic.js
@@ -1,12 +1,20 @@
+"use strict";
+// Topic list for the site homepage
+//
+// Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+// All Rights Reserved.
+//
+// This software is subject to the provisions of the Zope Public License,
+// Version 2.1 (ZPL). http://groupserver.org/downloads/license/
 jQuery.noConflict() 
 
 function gs_search_topics_site_homepage () {
-    var searchId = '#gs-search-topic-site-home-search', topicSearch = null;
+    var searchId='#gs-search-topic-site-home-search', topicSearch=null;
     topicSearch = GSSearch(searchId, '/s/search.ajax', 0, 10, {},  null);
     jQuery('#gs-site-home-activity-tabs li:first-child a').click();
     topicSearch.load();
 }
 jQuery(document).ready( function () {
-    gsJsLoader.with_module('/++resource++gs-search-base-js-min-20131121.js',
+    gsJsLoader.with_module('/++resource++gs-search-base-js-min-20140313.js',
                            gs_search_topics_site_homepage);
 });
