0a5f665e258d5c529297baa07d92183e565bbdbd
Michael JasonSmith <mpj17@onlinegroups.net>
2016-02-01 12:01:03 -0800

    Bumping the JavaScript version number

--- gs/search/topic/browser/javascript/loadtopic-min.js
+++ gs/search/topic/browser/javascript/loadtopic-min.js
@@ -1,4 +1,1 @@
-"use strict";jQuery.noConflict();function gs_search_topics_site_homepage(){var b="#gs-search-topic-site-home-search",a=null;
-a=GSSearch(b,"/s/search.ajax",0,10,{},null);jQuery("#gs-site-home-activity-tabs li:first-child a").click();
-a.load()}jQuery(document).ready(function(){gsJsLoader.with_module("/++resource++gs-search-base-js-min-20140313.js",gs_search_topics_site_homepage)
-});
+'use strict';jQuery.noConflict();function gs_search_topics_site_homepage(){var a=null,a=GSSearch("#gs-search-topic-site-home-search","/s/search.ajax",0,10,{},null);jQuery("#gs-site-home-activity-tabs li:first-child a").click();a.load()}jQuery(document).ready(function(){gsJsLoader.with_module("/++resource++gs-search-base-js-min-20140313.js",gs_search_topics_site_homepage)});
