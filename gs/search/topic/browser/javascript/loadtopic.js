00a7c6c9d51793903aa9abf51e53f3083e9fa51f
Michael JasonSmith <mpj17@onlinegroups.net>
2016-02-01 11:59:34 -0800

    Whitespace cleanup for gjslint

--- gs/search/topic/browser/javascript/loadtopic.js
+++ gs/search/topic/browser/javascript/loadtopic.js
@@ -1,20 +1,20 @@
-"use strict";
+'use strict';
 // Topic list for the site homepage
 //
-// Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+// Copyright © 2013, 2014, 2016 OnlineGroups.net and Contributors.
 // All Rights Reserved.
 //
 // This software is subject to the provisions of the Zope Public License,
 // Version 2.1 (ZPL). http://groupserver.org/downloads/license/
-jQuery.noConflict() 
+jQuery.noConflict();
 
-function gs_search_topics_site_homepage () {
-    var searchId='#gs-search-topic-site-home-search', topicSearch=null;
-    topicSearch = GSSearch(searchId, '/s/search.ajax', 0, 10, {},  null);
+function gs_search_topics_site_homepage() {
+    var searchId = '#gs-search-topic-site-home-search', topicSearch = null;
+    topicSearch = GSSearch(searchId, '/s/search.ajax', 0, 10, {}, null);
     jQuery('#gs-site-home-activity-tabs li:first-child a').click();
     topicSearch.load();
 }
-jQuery(document).ready( function () {
+jQuery(document).ready(function() {
     gsJsLoader.with_module('/++resource++gs-search-base-js-min-20140313.js',
                            gs_search_topics_site_homepage);
 });
